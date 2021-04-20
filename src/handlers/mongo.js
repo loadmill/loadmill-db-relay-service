8873a3b9ed4e2a3b65d954acb9f30a7d3a858b8b
Oron Bendavid <oronbdd@gmail.com>
2021-04-20 17:51:29 +0300

    added support to ALLOWED_DOMAINS when quering mongoDB

--- src/handlers/mongo.js
+++ src/handlers/mongo.js
@@ -1,6 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
 const runQuery = async (connectionString, collectionName, command, query = {}) => {
 
+  if (process.env.ALLOWED_DOMAINS) {
+    allowedDomains = process.env.ALLOWED_DOMAINS.split(',');
+    if (!allowedDomains.includes(getHost(connectionString))) {
+      throw { err: 'Domain is not allowed' };
+    }
+  }
+
   let client;
   try {
     client = await MongoClient.connect(connectionString);
@@ -13,7 +20,19 @@ const runQuery = async (connectionString, collectionName, command, query = {}) =
   finally {
     client.close();
   }
+}
 
+const getHost = connectionString => {
+  const regexExpression = connectionString.indexOf('@') >= 0 ? /(?<=(@))(.*)(?=:)/ : /(?<=\/\/)(.*)(?=:)/;
+  const tokens = regexExpression.exec(connectionString);
+
+  if (Array.isArray(tokens)) {
+    const host = tokens[0];
+    return host.indexOf(":") >= 0 ? host.split(':')[0] : host;
+  }
+  else {
+    throw { err: 'Connection string is not valid' }
+  }
 }
 
 module.exports = {
